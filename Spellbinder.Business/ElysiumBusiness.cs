e58f010c8c6a55c849aac0d36dae5d207cc8752d
DiegoLovera <diegolovera010@gmail.com>
2019-09-15 11:38:00 -0500

    fiexes

--- Spellbinder.Business/ElysiumBusiness.cs
+++ Spellbinder.Business/ElysiumBusiness.cs
@@ -1,5 +1,6 @@
 ﻿using Spellbinder.Models;
 using Spellbinder.Services.Elysium;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
--- Spellbinder.Services/Elysium/ElysiumService.cs
+++ Spellbinder.Services/Elysium/ElysiumService.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using Spellbinder.Models;
 using Spellbinder.Models.Configuration;
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -20,7 +21,7 @@ namespace Spellbinder.Services.Elysium
 
         public async Task<User> GetUser(string id)
         {
-            var uri = _elysiumConfig.BaseAddress + _elysiumConfig.User + "/" + id;
+            var uri = _elysiumConfig.BaseAddress + _elysiumConfig.Users + "/" + id;
             var responseString = await _httpClient.GetStringAsync(uri);
             return JsonConvert.DeserializeObject<User>(responseString);
         }

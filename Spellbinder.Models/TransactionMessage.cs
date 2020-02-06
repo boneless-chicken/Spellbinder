591af0896fc1f9418b057d97efa1e868711de9ea
Diego Lovera <diegolovera010@gmail.com>
2020-02-06 11:51:26 -0500

    test bestday

---
+++ Spellbinder.Models/TransactionMessage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spellbinder.Models
+{
+    public class TransactionMessage
+    {
+        public string Thing { get; set; }
+        public string TrxDate { get; set; }
+        public string TrxTime { get; set; }
+        public string TrxAmount { get; set; }
+        public string TrxReference { get; set; }
+        public string TrxRoomNbr { get; set; }
+        public string TrxCurrency { get; set; }
+        public string TrxPaymentMode { get; set; }
+        public string TrxUser { get; set; }
+        public string TrxFValue { get; set; }
+        public string TrxAuth { get; set; }
+        public string TrxResult { get; set; }
+        public string TrxResultCode { get; set; }
+        public string TrxDescription { get; set; }
+        public string TrxCard { get; set; }
+        public string TrxOrgNumber { get; set; }
+        public string TrxMerchant { get; set; }
+        public string TrxAID { get; set; }
+        public string TrxArqc { get; set; }
+        public string TrxCardBrand { get; set; }
+        public string TrxCardBank { get; set; }
+        public string TrxCardInstrument { get; set; }
+    }
+}
---
+++ Spellbinder/Controllers/TransactionsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Spellbinder.Models;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace Spellbinder.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> TransactionStatus(TransactionMessage message)
+        {
+            try
+            {
+                string jsonData = JsonConvert.SerializeObject(message);
+                SmtpClient client;
+                using (client = new SmtpClient("smtp.office365.com", 587))
+                {
+                    client.UseDefaultCredentials = false;
+                    client.EnableSsl = true;
+                    client.Timeout = 60;
+                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    client.Credentials = new NetworkCredential("diego.lovera@bestday.com", "T3mporal1");
+
+                    MailMessage mailMessage = new MailMessage
+                    {
+                        From = new MailAddress("diego.lovera@bestday.com")
+                    };
+                    mailMessage.To.Add("diego.lovera@bestday.com");
+                    mailMessage.Body = jsonData;
+                    mailMessage.Subject = "Mensaje de transacción";
+                    await client.SendMailAsync(mailMessage);
+                };
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
